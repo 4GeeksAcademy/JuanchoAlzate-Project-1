346966ce2f93014935c9724a24a7b628e558966a
Juan Alzate <139303318+juanchoalzate@users.noreply.github.com>
2024-01-28 20:09:52 +0000

    28-01-2024-add

--- js/machine.js
+++ js/machine.js
@@ -1,11 +1,29 @@
 var StatusMachine = "off";
+var machine = document.getElementById("blender");
+var SoundMachine = document.getElementById("blender-sound");
+var ButtomMachine = document.getElementById("blender-buttom-sound");
 
 function ControlMachine(){
     if (StatusMachine == "off") {
         StatusMachine = "on";
+        AudioMachine();
+        machine.classList.add("active");
         console.log("Maquina Encendida");
     } else {
-        StatusMachine == "off";
+        StatusMachine = "off";
+        AudioMachine();
+        machine.classList.remove("active")
         console.log("Maquina Apagada");
+    }
+}
+
+function AudioMachine(){
+    if (SoundMachine.paused) {
+        ButtomMachine.play();
+        SoundMachine.play();
+    } else {
+        ButtomMachine.play();
+        SoundMachine.pause();
+        SoundMachine.currentType = 0;
     }
 }
